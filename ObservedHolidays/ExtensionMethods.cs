f45561e1b10966e131b1bf2c1063492dbf48e1f2
J.D. Cain <jd.cain.jr@gmail.com>
2018-02-10 09:07:55 -0500

    moved data typsto their own file.

--- ObservedHolidays/ExtensionMethods.cs
+++ ObservedHolidays/ExtensionMethods.cs
@@ -4,26 +4,6 @@ using System.Linq;
 
 namespace ObservedHolidays
 {
-    public class HolidayData
-    {
-        public IEnumerable<StaticHoliday> Static { get; set; }
-        public IEnumerable<VariableHoliday> Variable { get; set; }
-    }
-    public class StaticHoliday
-    {
-        public Holiday Holiday { get; set; }
-        public int Month { get; set; }
-        public int Day { get; set; }
-    }
-    public class VariableHoliday
-    {
-        public Holiday Holiday { get; set; }
-        public int Month { get; set; }
-        public DayOfWeek DayOfWeek { get; set; }
-        public int Position { get; set; }
-        public bool Reverse { get; set; }
-    }
-
     public static class ExtensionMethods
     {
         private static readonly HolidayData _defaultHolidays = new HolidayData()
---
+++ ObservedHolidays/HolidayDataTypes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ObservedHolidays
+{
+    public class HolidayData
+    {
+        public IEnumerable<StaticHoliday> Static { get; set; }
+        public IEnumerable<VariableHoliday> Variable { get; set; }
+    }
+    public class StaticHoliday
+    {
+        public Holiday Holiday { get; set; }
+        public int Month { get; set; }
+        public int Day { get; set; }
+    }
+    public class VariableHoliday
+    {
+        public Holiday Holiday { get; set; }
+        public int Month { get; set; }
+        public DayOfWeek DayOfWeek { get; set; }
+        public int Position { get; set; }
+        public bool Reverse { get; set; }
+    }
+}
